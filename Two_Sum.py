04d01dce8bbb56172f27bd0de78a152b4b58af95
Swaminathan B <78146309+swami1302@users.noreply.github.com>
2022-12-16 23:21:40 +0530

    Create Two_Sum.py

---
+++ Two_Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d={}
+        for i in range(len(nums)):
+            new = target-nums[i]
+            if new in d:
+                return [d[new],i]
+                
+            else:
+                d[nums[i]]=i

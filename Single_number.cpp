ffd13e0b8bd544c55998710c0e5636c7770190be
Swaminathan B <78146309+swami1302@users.noreply.github.com>
2022-12-18 21:13:06 +0530

    Create Single_number.cpp

---
+++ Single_number.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/single-number/description/
+// TC: O(nlogn)
+// SC: O(1)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-1;i+=2){
+            if(nums[i]!=nums[i+1])
+            return nums[i];
+        }
+        return nums[n-1];
+    }
+};
